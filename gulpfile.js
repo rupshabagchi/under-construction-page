34ee2867349a000e591e845a4849c4b42dcc77ef
rupshabagchi <rupsha.bagchi@gmail.com>
2017-06-20 12:10:54 +0300

    gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,8 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('uglify');
-var watchify = require('watchify');
+var cssminify = require('gulp-minify-css');
 
-gulp.task('switcher', function() {
-  gulp.src('css/*.css')
-  .pipe(concat('switcher.min.css'))
-  .pipe(gulp.dest('dist/'))
+gulp.task('default', function() {
+  gulp.src('css/colors*.css')
+  .pipe(cssminify())
+  .pipe(gulp.dest('./dist/'))
 });
-
-gulp.task('css', function () {
-    gulp.watch('css/*.css', function () {
-        return gulp.src('css/*.css')
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('dist/'));
-    });
-});
-
-// Just running the two tasks
-gulp.task('default', ['switcher', 'css']);
